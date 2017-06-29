2712b09b60f41dde91e3d0a73e201949a80d745c
bram <bramkaashoek@gmail.com>
2017-06-29 15:46:01 +0200

    rewrite algorithm

--- src/actions/batches/random-student.js
+++ src/actions/batches/random-student.js
@@ -2,16 +2,44 @@ export const RANDOM_STUDENT = 'RANDOM_STUDENT'
 
 export default (students, batchId, batchNumber) => {
   return (dispatch) => {
-  const evalArray = students.map((s) => s.evaluations[s.evaluations.length - 1].color)
-  let weightedStudents = []
-  for ( var t = 0; t < (students.length); t++ ){
-    for ( var i = evalArray[t]; i >= 0; i-- ){
-      weightedStudents = weightedStudents.concat(students[t])
+
+  function getStudent(category){
+    if (category < 3){
+      const selected = selectFromColor(0)
+      if (selected.length === 0){
+        const newCat = Math.floor(Math.random() * 3) + 3
+        return getStudent(newCat)
+      }
+      return buildStudent(selected)
+    } else if (category < 5) {
+      const selected = selectFromColor(1)
+      if (selected.length === 0){
+        const newCat = 5
+        return getStudent(newCat)
+      }
+      return buildStudent(selected)
+    } else {
+      const selected = selectFromColor(2)
+      if (selected.length === 0){
+        return null
+      }
+      return buildStudent(selected)
     }
   }
-  const student = weightedStudents[Math.floor(Math.random() * (weightedStudents.length))]
-  student.batchId = batchId
-  student.batchNumber = batchNumber
+
+  function selectFromColor(color){
+    return students.filter((s) => { return s.evaluations[s.evaluations.length - 1].color === color })
+  }
+
+  function buildStudent(selected){
+    const student = selected[Math.floor(Math.random() * selected.length)]
+    student.batchId = batchId
+    student.batchNumber = batchNumber
+    return student
+  }
+
+  const firstCategory = Math.floor(Math.random() * 6)
+  const student = getStudent(firstCategory)
 
     dispatch({
       type: RANDOM_STUDENT,
